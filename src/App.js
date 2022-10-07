ede65dfbb0b1a657b6601592e6953ccb0ebdbb83
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-10-07 15:29:57 +0200

    working oin it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
+import ButtonPanel from './ButtonPanel';
+import Title from './Title';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: null
+    }
+  }
+
+  changeColor = (event) => {
+    this.setState((prevState) => {
+      let changedColor = prevState.color;
+
+      if(event === 'red') {
+        console.log('Kolor red')
+      } else if (event === 'blue') {
+        console.log('Kolor blue')
+      } else {
+        console.log('Kolor green')
+      }
+
+      return ({
+        color: changedColor
+      })
+
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Title Color App</h1>
+        </header>
+        <Title />
+        <ButtonPanel changeColor={this.changeColor}/>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
